48749187245429b49f02fdc3854196ada1b5c68a
Your Name <s1f102301820@iniad.org>
2025-05-11 19:00:43 +0900

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NoteDetail from "./pages/NoteDetail";
+import Home from "./pages/Home";
+import Layout from "./Layout";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import "./App.css";
+
 function App() {
   return (
-    <>
-      <p>hello world</p>
-    </>
+    <BrowserRouter>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/notes/:id" element={<NoteDetail />} />
+          </Route>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+import react from "@vitejs/plugin-react";
+module.exports = {
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
