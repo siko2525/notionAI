3b396901d1f077b06831fefc9467c8ec121de23e
Your Name <s1f102301820@iniad.org>
2025-06-01 21:56:30 +0900

    correction

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,7 @@
 import { useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/mantine/style.css";
 import { BlockNoteView } from "@blocknote/mantine";
-import { ja } from "@blocknote/core/locales";
+import { locales } from "@blocknote/core";
 
 interface EditorProps {
   onChange: (value: string) => void;
@@ -10,10 +10,11 @@ interface EditorProps {
 
 function Editor({ onChange, initialContent }: EditorProps) {
   const editor = useCreateBlockNote({
-    dictionary: ja,
+    dictionary: locales.ja,
     initialContent:
       initialContent != null ? JSON.parse(initialContent) : undefined,
   });
+
   return (
     <div>
       <BlockNoteView
